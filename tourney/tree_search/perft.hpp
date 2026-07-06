26f0aeb08ad93d7b36e547d021e2e0826a153e36
Elijah Kin <elijah.m.kin@gmail.com>
2026-07-05 18:18:30 -0700

    Big perft speedup

--- tourney/tree_search/perft.hpp
+++ tourney/tree_search/perft.hpp
@@ -1,5 +1,11 @@
 #include <cstddef>
 
+// Perft (performance test, move path enumeration) simply walks over a game tree
+// to count all the leaf nodes of a certain depth. It is primarily used for
+// ensuring the correctness and measuring the performance of move generation.
+//
+// https://www.chessprogramming.org/Perft
+
 template <typename State>
 class PerftSearch {
  private:
@@ -10,19 +16,29 @@ class PerftSearch {
  public:
   explicit PerftSearch(State& state) : state_(state) {}
 
-  // Recursively count nodes for debugging move generation
   size_t search(size_t depth) {
+    // Depth 0 corresponds to only the root node.
     if (depth == 0) {
       return 1;
     }
 
+    Move moves[Move::kMaxMoves];
+    size_t n = state_.FillLegalMoves(moves, Move::kMaxMoves);
+
+    // For depth 1, just return the number of legal moves. Note that this
+    // condition is technically unnecessary, but eliminates the cost of
+    // MakeMove, UnmakeMove, and recursion.
+    if (depth == 1) {
+      return n;
+    }
+
+    // Otherwise, we iterate over the moves, making each one, recursing, and
+    // unmaking it before moving on to the next.
     size_t nodes = 0;
-    Move buf[Move::kMaxMoves];
-    size_t n = state_.FillLegalMoves(buf, Move::kMaxMoves);
     for (size_t i = 0; i < n; ++i) {
-      state_.MakeMove(buf[i]);
+      state_.MakeMove(moves[i]);
       nodes += search(depth - 1);
-      state_.UnmakeMove(buf[i]);
+      state_.UnmakeMove(moves[i]);
     }
     return nodes;
   }
